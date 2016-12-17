8cbd5af821996a085da16477bfdc1acfb32d8912
treelite <c.xinle@gmail.com>
2016-12-17 22:34:05 +1100

    Fix callback & exclude files

--- test/traverseSpec.js
+++ test/traverseSpec.js
@@ -11,8 +11,8 @@ describe('traverse', () => {
     it('normal', async (done) => {
         let dir = path.resolve(__dirname, '../../test/sample');
         let files = [];
-        await traverse(dir, file => files.push(file));
-        expect(files.length).toEqual(3);
+        await traverse(dir, [path.resolve(dir, 'src/util')], file => files.push(file));
+        expect(files.length).toEqual(5);
         done();
     });
 
